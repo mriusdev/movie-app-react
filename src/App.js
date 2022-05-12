b20fe3f3cd1c331dea581efedc8ba60889903da3
mriusdev <93098512+mriusdev@users.noreply.github.com>
2022-05-12 13:16:06 +0300

    cleaned up some code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Flex } from '@chakra-ui/react'
 
-
 import Header from "./components/Header/Header";
-// import About from './pages/About'
-// import Home from './pages/Home'
-// import SearchedMovies from './pages/SearchedMovies';
-// import Movie from './pages/Movie'
 import Footer from './components/Footer';
 import AnimatedRoutes from './components/AnimatedRoutes';
-
 
 function App() {
   return (
